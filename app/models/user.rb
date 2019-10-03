595a41820d334b16dcb6f3fdb79f18d655e6842a
Audrea Cook <audreacwork@gmail.com>
2019-10-03 15:30:04 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
-
-  validates :password, presence: true
+  has_secure_password
 
   def mood
-    nausea > happiness ? "sad" : "happy"
+    nausea > happiness ? 'sad' : 'happy'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_10_03_202927) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.integer "nausea"
     t.integer "happiness"
     t.integer "tickets"
